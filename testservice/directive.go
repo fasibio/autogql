e97958a785473a0213c28ccb705d19e0788f4916
Fabian Simon <fabian.simon@inovex.de>
2023-07-21 20:23:12 +0200

    fix: no sql type generation logik

--- testservice/directive.go
+++ testservice/directive.go
@@ -6,11 +6,12 @@ import (
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/fasibio/autogql/testservice/graph/model"
+	"github.com/go-playground/validator/v10"
+	"github.com/vektah/gqlparser/v2/gqlerror"
 )
 
 func ValidateDirective(ctx context.Context, obj interface{}, next graphql.Resolver) (res interface{}, err error) {
 	field := graphql.GetPathContext(ctx)
-
 	if data, ok := obj.(map[string]interface{}); ok {
 		for _, v := range field.ParentField.Field.Arguments {
 			model, err := model.GetInputStruct(v.Value.ExpectedType.Name(), data)
@@ -18,7 +19,31 @@ func ValidateDirective(ctx context.Context, obj interface{}, next graphql.Resolv
 				continue
 			}
 			if err := validate.Struct(model); err != nil {
-				return nil, fmt.Errorf("validationerror:  %v", err)
+				switch err := err.(type) {
+				case validator.ValidationErrors:
+					{
+						errList := gqlerror.List{}
+						for _, v := range err {
+
+							errList = append(errList, &gqlerror.Error{
+								Path:    graphql.GetPath(ctx),
+								Message: v.Error(),
+								Extensions: map[string]interface{}{
+									"code":      "INPUT_VALIDATION_ERROR",
+									"tag":       v.Tag(),
+									"field":     v.Field(),
+									"namespace": v.Namespace(),
+								},
+							})
+						}
+						return nil, errList
+					}
+				default:
+					{
+						return nil, fmt.Errorf("validationerror:  %v", err)
+					}
+				}
+
 			}
 		}
 	}
